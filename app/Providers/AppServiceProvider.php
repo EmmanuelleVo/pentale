bbe683c6c61d4996cfcfd7dcca8ca9a75918885d
EmmanuelleVo <57753923+EmmanuelleVo@users.noreply.github.com>
2023-06-20 11:29:01 +0200

    [ADD] - https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        URL::forceScheme('https');
         if ($this->app->environment('production')) {
             URL::forceScheme('https');
         }
